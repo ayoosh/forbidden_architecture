5f06669936588d0d8cc6e4ce326c11ec26cc44a2
vchandrasek3 <vchandrasek3@wisc.edu>
2014-04-10 15:44:13 -0500

    Synthesized code - Cache_Controller

---
+++ CPU/Cache_Controller/Working_synthesis/cache_controller_t.v
@@ -0,0 +1,179 @@
+`timescale 1ns/1ps
+
+module cache_controller_t;
+	reg					clk;
+	reg					rst_n;
+	reg		[27:0]		cache_addr;
+	reg		[31:0]		cache_wr;
+	reg					cache_rw;
+	reg					cache_valid;
+	reg					flush;
+	reg		[255:0]		mem_rd;
+	reg			 		mem_ready;
+
+	reg		[31:0] 		ram	[0:4];
+	reg		[11*8:0]	StateName;
+
+	integer				ram_ptr, i;
+
+	wire	[31:0]		cache_rd;
+	wire				cache_ready;
+	wire	[27:0]		mem_addr;
+	wire	[255:0]		mem_wr;
+	wire				mem_rw;
+	wire				mem_valid;
+
+	initial begin
+		clk = 0;
+		ram_ptr = 0;
+		i=0;
+		flush = 0;
+		rst_n = 0;
+		$monitor ("StateName : %s", StateName);
+		for (i=0; i<5; i=i+1)
+			ram[i] = 0;
+	end
+
+	cache_controller dut (
+		.clk 			(clk),
+		.rst_n 			(rst_n),
+		.cache_addr		(cache_addr),
+		.cache_wr		(cache_wr),
+		.cache_rw		(cache_rw),
+		.cache_valid	(cache_valid),
+		.flush			(flush),
+		.mem_rd			(mem_rd),
+		.mem_ready		(mem_ready),
+
+		.cache_rd		(cache_rd),
+		.cache_ready	(cache_ready),
+		.mem_addr		(mem_addr),
+		.mem_wr			(mem_wr),
+		.mem_rw			(mem_rw),
+		.mem_valid_out	(mem_valid)
+	);
+	
+	initial begin
+		cache_addr = 1'b0;
+		cache_wr = 32'h0000_0000;
+		cache_rw = 1'b0;
+		cache_valid = 1'b0;
+		
+		#20
+		rst_n = 1;
+		#5
+		
+		for (i = 0; i < 1024; i = i + 1) begin
+			dut.gen_way[0].memory.memory[i] = 273'h1;
+			dut.gen_way[1].memory.memory[i] = 273'h0;
+		end
+		#10
+
+		// Make WRITE condition TRUE
+		cache_valid = 1'b1;
+		cache_rw = 1'b1;
+
+		cache_addr = 28'd0;
+		cache_wr = 32'hABCD_1234;
+		ram[ram_ptr] = cache_wr;
+		ram_ptr = ram_ptr + 1;
+
+		#10
+		cache_addr = 28'd8;
+		cache_wr = 32'h1234_5678;
+		ram[ram_ptr] = cache_wr;
+		ram_ptr = ram_ptr + 1;
+
+		#10
+		cache_addr = 28'd16;
+		cache_wr = 32'h5678_8765;
+		ram[ram_ptr] = cache_wr;
+		ram_ptr = ram_ptr + 1;
+
+		#10
+		cache_addr = 28'd24;
+		cache_wr = 32'h8765_4321;
+		ram[ram_ptr] = cache_wr;
+		ram_ptr = ram_ptr + 1;
+
+		#10
+		cache_addr = 28'd32;
+		cache_wr = 32'h4321_DCBA;
+		ram[ram_ptr] = cache_wr;
+		ram_ptr = ram_ptr + 1;
+		
+		#10
+		cache_valid = 1'b0;
+
+		#50
+		// Make READ condition TRUE
+		cache_valid = 1'b1;
+		cache_rw = 1'b0;
+		ram_ptr = 0;
+		cache_addr = 28'd0;
+		
+		#10
+		$display ("Data 1: %h", cache_rd);
+		if (cache_rd == ram[ram_ptr])
+			$display ("Data 1 is correct");
+		else
+			$display ("Data 1 is wrong. Expected value is ram: %h", ram[ram_ptr]);
+		ram_ptr = ram_ptr + 1;
+
+		cache_addr = 28'd8;
+		#10
+		$display ("Data 1: %h", cache_rd);
+		if (cache_rd == ram[ram_ptr])
+			$display ("Data 1 is correct");
+		else
+			$display ("Data 1 is wrong. Expected value is ram: %h", ram[ram_ptr]);
+		ram_ptr = ram_ptr + 1;
+
+		cache_addr = 28'd16;
+		#10
+		$display ("Data 1: %h", cache_rd);
+		if (cache_rd == ram[ram_ptr])
+			$display ("Data 1 is correct");
+		else
+			$display ("Data 1 is wrong. Expected value is ram: %h", ram[ram_ptr]);
+		ram_ptr = ram_ptr + 1;
+
+		cache_addr = 28'd24;
+		#10
+		$display ("Data 1: %h", cache_rd);
+		if (cache_rd == ram[ram_ptr])
+			$display ("Data 1 is correct");
+		else
+			$display ("Data 1 is wrong. Expected value is ram: %h", ram[ram_ptr]);
+		ram_ptr = ram_ptr + 1;
+
+		cache_addr = 28'd32;
+		#10
+		$display ("Data 1: %h", cache_rd);
+		if (cache_rd == ram[ram_ptr])
+			$display ("Data 1 is correct");
+		else
+			$display ("Data 1 is wrong. Expected value is ram: %h", ram[ram_ptr]);
+		ram_ptr = ram_ptr + 1;
+		
+		#10
+		$stop();
+	end
+
+
+
+	always @ (dut.State) begin
+		case (dut.State)
+			2'b00: StateName = "IDLE";
+			2'b01: StateName = "COMPARE_TAG";
+			2'b10: StateName = "WRITE_BACK";
+			2'b11: StateName = "ALLOCATE";
+			default:StateName = "??";
+		endcase
+	end
+
+
+	always begin
+		#5 clk = ~clk;
+	end
+endmodule
---
+++ CPU/Cache_Controller/Working_synthesis/cache_memory.v
@@ -0,0 +1,81 @@
+`timescale 1ns/1ps
+
+module cache_memory #(
+	parameter	ADDR_WIDTH	= 28,
+	parameter	DATA_WIDTH	= 32,
+	parameter	BLOCK_SIZE	= 256,
+	parameter	CACHE_SIZE	= 65536
+)
+(
+	// Outputs
+	output		[BLOCK_SIZE-1:0]	data_read,
+	output							dirty_read,
+	output							hit,
+	
+	// Inputs
+	input		[ADDR_WIDTH-1:0]	addr,
+	input		[BLOCK_SIZE-1:0]	data_write,
+	input							dirty_write,
+	input							write_en,
+	input							clk,
+	input							rst_n
+);
+
+	function integer log2(input integer value);
+		begin
+			value = value - 1;
+			for (log2 = 0; value > 0; log2 = log2 + 1) begin
+				value = value >> 1;
+			end
+		end
+	endfunction
+
+	localparam	NUM_BLOCKS		= (CACHE_SIZE * 8) / BLOCK_SIZE;
+	localparam	DATA_BLOCKS		= BLOCK_SIZE / DATA_WIDTH;
+	localparam	OFFSET_WIDTH	= log2(DATA_BLOCKS);
+	localparam	INDEX_WIDTH		= log2(NUM_BLOCKS);
+	localparam	TAG_WIDTH		= ADDR_WIDTH - INDEX_WIDTH - OFFSET_WIDTH;
+	localparam	MEMORY_SIZE		= BLOCK_SIZE + TAG_WIDTH + 1 + 1; // 1 for valid and 1 for dirty
+	
+	//(* RAM_STYLE="BLOCK" *)
+	reg		[MEMORY_SIZE-1:0]	memory[NUM_BLOCKS-1:0];
+	
+	wire	[TAG_WIDTH-1:0]		addr_tag;
+	wire	[INDEX_WIDTH-1:0]	addr_index;
+	wire	[OFFSET_WIDTH-1:0]	addr_offset;
+	reg	[BLOCK_SIZE-1:0]	data;
+	reg	[TAG_WIDTH-1:0]		tag;
+	reg						dirty;
+	reg						valid;
+	
+	//integer						i;
+	
+	assign addr_tag		= addr[ADDR_WIDTH-1:ADDR_WIDTH-TAG_WIDTH];
+	assign addr_index		= addr[ADDR_WIDTH-TAG_WIDTH-1:ADDR_WIDTH-TAG_WIDTH-INDEX_WIDTH];
+	assign addr_offset	= addr[OFFSET_WIDTH-1:0];
+	
+	assign data_read	= data;
+	assign dirty_read	= dirty;
+	assign hit			= valid & (addr_tag == tag);
+	
+	always @ (negedge clk) begin
+		if(!rst_n) begin
+			data		<= 0;
+			tag		<= 0;
+			dirty		<= 0;
+			valid		<= 0;
+			//for (i = 0; i < NUM_BLOCKS; i = i + 1)
+			//	memory[i] <= 273'h0;
+		end
+		else begin
+			//i = 0;
+			data		<= memory[addr_index][MEMORY_SIZE-1:MEMORY_SIZE-BLOCK_SIZE];
+			tag		<= memory[addr_index][MEMORY_SIZE-BLOCK_SIZE-1:2];
+			dirty		<= memory[addr_index][1];
+			valid		<= memory[addr_index][0];
+			
+			if (write_en)
+				memory[addr_index] <= {data_write, addr_tag, dirty_write, 1'b1};
+		end
+	end
+endmodule
