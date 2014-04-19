85dd56d6f43af1d69adb9c33bd1f7b3d5e57f703
Rohit Shukla <rshukla3@wisc.edu>
2014-04-19 12:41:11 -0500

    Uploaded the SPART codes in correct folder
    The working codes of SPART have been uploaded to the correct folder
    inside MMU. Only thing that needs to be done is check it using MATLAB.

--- MMU/SPART_cache_kushagra/tb_top.v
+++ MMU/SPART_cache_kushagra/tb_top.v
@@ -1,59 +1,96 @@
 `timescale 1ns/1ps
 
-module top_level(
-    input clk,         // 100mhz clock
-    input rst,         // Asynchronous reset, tied to dip switch 0
-    output txd,        // RS232 Transmit Data
-    input rxd        // RS232 Receive Data
-    );
-	
-	wire   [31:0] mem_data_wr1;
-								// Input data from cache
-								
-	wire   [31:0] mem_data_rd1;
-								// Output the data to cache
-								
-    wire     [27:0]  mem_data_addr1;
-								// DDR2 address from cache to read/write the data from/to
-								
-	wire      mem_rw_data1;
-	                     // To indicate read/write command from cache
-								
-	wire      mem_valid_data1;
-	                     // To indicate from cache if command is valid
-								
-	wire	  mem_ready_data1;
+module tb_top;
+reg clk, rst, RX;
+reg [1:0] br_cfg;
+wire txd;
 
-	Dcache_dummy proc (
-		.clk(clk), 
-		.rst(rst), 
-		.mem_data_wr1(mem_data_wr1), 
-		.mem_data_rd1(mem_data_rd1), 
-		.mem_data_addr1(mem_data_addr1), 
-		.mem_rw_data1(mem_rw_data1), 
-		.mem_valid_data1(mem_valid_data1), 
-		.mem_ready_data1(mem_ready_data1)
-	);
+top_level U0 (
+.clk  (clk),
+.rst  (rst),
+.txd (txd),
+.rxd (RX),
+.br_cfg (br_cfg)
+);
 
-// Instantiate SPART here
 
-	spart_top_level spart_interface (
-		.clk(clk),         // 100mhz clock
-		.rst(rst),         // Asynchronous reset, tied to dip switch 0
-		.txd(txd),        // RS232 Transmit Data
-		.rxd(rxd),         // RS232 Receive Data
+
+initial
+begin
+clk = 0;
+rst = 1;
+RX = 1;
+br_cfg = 2'b01;
+
+#40 rst = 0;
+
+#6500;
+RX = 0;		
+
+
+#104310;
+RX = 1;		
+
+#104310;
+RX = 0;		
+
+#104310;
+RX = 1;
+
+#104310;
+RX = 0;
+
+#104310;
+RX = 1;
+
+#104310;
+RX = 0;		
+
+#104310;
+RX = 1;		
+
+#104310;
+RX = 0;
 	
-	// Clocks from DDR2 for ChipScope
-//	input clk0_tb,    
-//	input clk200_out,
+#104310;
+RX = 1;	
+
+
+// Next Read command
+#6500000;
+RX = 0;		
+
+
+#104310;
+RX = 0;		
+
+#104310;
+RX = 1;		
+
+#104310;
+RX = 0;
+
+#104310;
+RX = 1;
+
+#104310;
+RX = 0;
+
+#104310;
+RX = 1;		
+
+#104310;
+RX = 0;		
+
+#104310;
+RX = 1;
 	
-	// Signals from/to SPART Cache interface
-		.io_rw_data(mem_rw_data1),
-		.io_valid_data(mem_valid_data1),
-		.io_ready_data(mem_ready_data1),
-		.mem_addr(mem_data_addr1),
-		.io_rd_data(mem_data_rd1),
-		.io_wr_data(mem_data_wr1)
-	);
+#104310;
+RX = 1;	
+
+end
+
+always 
+#5 clk = ~clk;
 
 endmodule
