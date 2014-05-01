b06807b5e978714229e1715f2e1af4dce666f132
Rohit Shukla <rshukla3@wisc.edu>
2014-04-30 23:14:04 -0500

    MATLAB codes for SPART connection
    This commit contains the MATLAB codes for connecting FPGA to computer
    through SPART.

---
+++ MATLAB SPART connection/serial_comm_Write_then_read.m
@@ -0,0 +1,30 @@
+clc;
+clear all;
+s = serial('COM1');
+set(s,'BaudRate',38400);
+% set(s, 'InputBufferSize', 1);
+% set(s, 'OutputBufferSize', 1);
+set(s,'DataBits',8);
+set(s,'StopBit',1);
+set(s,'Parity', 'none');
+set(s, 'FlowControl', 'none');
+
+fopen(s);
+%out = get(s,'Timeout');
+for i = 1:1
+    fwrite(s,i, 'uint32');
+end
+
+for i = 1:1
+    [out1, count1, msg1] = fread(s,1, 'uint32');
+    fprintf('out1 = %d\n', out1);
+    if(out1 ~= i)
+        fprintf('Error!! Values did not match');
+        break;
+    end
+end
+
+fclose(s)
+delete(s)
+clear s
+fprintf('This part was executed\n');
