d18eba4f517b91d358a6a30a84783db45a4eff6f
kushagragarg <kushagragarg.bits@gmail.com>
2014-05-06 04:18:25 -0500

    50 MHZ working setup

---
+++ Final_Top_level_CPU_NPU_Interfaces_50Mhz/chipscope_modules/ila.v
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2014 Xilinx, Inc.
+// All Rights Reserved
+///////////////////////////////////////////////////////////////////////////////
+//   ____  ____
+//  /   /\/   /
+// /___/  \  /    Vendor     : Xilinx
+// \   \   \/     Version    : 14.2
+//  \   \         Application: Xilinx CORE Generator
+//  /   /         Filename   : ila.v
+// /___/   /\     Timestamp  : Wed Apr 23 02:01:31 Central Daylight Time 2014
+// \   \  /  \
+//  \___\/\___\
+//
+// Design Name: Verilog Synthesis Wrapper
+///////////////////////////////////////////////////////////////////////////////
+// This wrapper is used to integrate with Project Navigator and PlanAhead
+
+`timescale 1ns/1ps
+
+module ila(
+    CONTROL,
+    CLK,
+    DATA,
+    TRIG0);
+
+
+inout [35 : 0] CONTROL;
+input CLK;
+input [999 : 0] DATA;
+input [7 : 0] TRIG0;
+
+endmodule
