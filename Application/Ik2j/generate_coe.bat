f3dada515c96c57ee14d04552138af6679161984
Ayoosh Bansal <ayoosh4@gmail.com>
2014-05-06 13:12:20 -0500

    application codes n cores

---
+++ Application/Ik2j/generate_coe.bat
@@ -0,0 +1,6 @@
+del machine_code.txt
+del reference.txt
+del Sobeltadumdumdum.coe
+perl ..\Assembler\cpu\assembler_for_cpu.pl Sobeltadumdumdum.txt
+rename machine_code.txt Sobeltadumdumdum.coe
+REM rename machine_code.txt Img_commanded_Show.coe
---
+++ Application/Sobel/1img_algo_npu_commanded_show_withtics/generate_coe.bat
@@ -0,0 +1,7 @@
+del machine_code.txt
+del reference.txt
+del Sobeltadumdumdum_withtics.txt
+del Sobeltadumdumdum_withtics.coe
+perl ..\..\..\Assembler\cpu\assembler_for_cpu.pl Sobeltadumdumdum_withtics.pl
+rename machine_code.txt Sobeltadumdumdum_withtics.coe
+rename reference.txt Sobeltadumdumdum_withtics.txt
