92f82c4917b520cef1b6fec53f7fd7f749daf1a9
Ayoosh Bansal <ayoosh4@gmail.com>
2014-05-08 03:18:58 -0500

    npu fix and appls

--- NPU/coregens/blk_mem_gen_v6_1.vhd
+++ NPU/coregens/blk_mem_gen_v6_1.vhd
@@ -69,7 +69,7 @@ END COMPONENT;
       c_axi_type => 1,
       c_byte_size => 9,
       c_common_clk => 0,
-      c_default_data => "80",
+      c_default_data => "F",
       c_disable_warn_bhv_coll => 0,
       c_disable_warn_bhv_range => 0,
       c_family => "virtex5",
